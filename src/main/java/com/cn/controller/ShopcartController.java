4b888dc571a653c7ee94eae8de238e6f10a7b341
Administrator <yixin316@foxmail.com>
2019-06-26 10:15:41 +0800

    商品插入

--- src/main/java/com/cn/controller/ShopcartController.java
+++ src/main/java/com/cn/controller/ShopcartController.java
@@ -1,10 +1,13 @@
 package com.cn.controller;
 
+import com.cn.model.Merchandise;
 import com.cn.model.Shopcart;
 import com.cn.model.User;
 import com.cn.service.IMerchandiseService;
 import com.cn.service.impl.ShopcartServiceImpl;
 import com.cn.tools.Constants;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,7 +41,7 @@ public class ShopcartController {
             uid = user.getUid();
         }else
         {
-            return "redirect:/login.html";
+            return "false";
         }
         mid=Long.parseLong(request.getParameter("mid"));
         snum=Integer.valueOf(request.getParameter("snum"));
@@ -66,5 +69,38 @@ public class ShopcartController {
         List<Shopcart> shoplist=shopcartService.selectShopByUID(user.getUid());
 
 
+    }
+    public JSONObject getShopJson(List<Shopcart> shoplist) {
+//        JSONArray merchs=new JSONArray();
+//        JSONObject json=new JSONObject();
+//        for(int i=0;i<shoplist.size();i++)
+//        {
+////
+////                long mid = merchsAll.get(i).getMid();
+////                String mname = merchsAll.get(i).getMname();
+////                String mimage = merchsAll.get(i).getMimage();
+////                int mprice = merchsAll.get(i).getMprice();
+////                int mstock = merchsAll.get(i).getMstock();
+////                String mtype = merchsAll.get(i).getMtype();
+////                String mtime = merchsAll.get(i).getMtime();
+////                String mrecommend = merchsAll.get(i).getMrecommend();
+//
+//            JSONObject member_temp=new JSONObject();
+//
+//            member_temp.put("mid", merchsAll.get(i).getMid());
+//            member_temp.put("mname", merchsAll.get(i).getMname());
+//            member_temp.put("mimage", merchsAll.get(i).getMimage());
+//            member_temp.put("mprice", merchsAll.get(i).getMprice());
+//            member_temp.put("mstock", merchsAll.get(i).getMstock());
+//            member_temp.put("mtype", merchsAll.get(i).getMtype());
+//            member_temp.put("mtime", merchsAll.get(i).getMtime());
+//            member_temp.put("mrecommend", merchsAll.get(i).getMrecommend());
+//
+//            merchs.add(member_temp);
+//        }
+//
+//        json.put("allMerchandise", merchs);
+//        json.put("num",merchsAll.size());
+        return null;
     }
 }
