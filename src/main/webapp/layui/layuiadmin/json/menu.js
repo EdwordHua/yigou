54116073acf9f3ab3e1972acdffd95d776686fe0
徐斌 <17786302556@qq.com>
2019-06-26 19:28:55 +0800

    1212121

--- src/main/webapp/layui/layuiadmin/json/menu.js
+++
@@ -1,6 +0,0 @@
-response: { //定义后端 json 格式，详细参见官方文档
-  code: 0,
-      msg: "",
-      count: 1000,
-      data: ["mname":"小米8","mrecommend":"很流畅，性价比很高，推荐购买！","mstock":888,"mimage":null,"mid":1,"mtype":"手机","mtime":"2019-6-20","mprice":3100]
-}
